df4ccfb0eaed3a56aae7e26b526707df945e44f9
Ananthu M <ananthu.m.utr@gmail.com>
2025-05-04 15:11:35 +0530

    categories update

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -1,10 +1,13 @@
 import React from 'react'
+import BestSeller from '../components/BestSeller'
+import Categories from '../components/Categories'
 import MainBanner from '../components/MainBanner'
-
 const Home = () => {
   return (
     <div className='mt-10'>
-        <MainBanner></MainBanner>
+        <MainBanner/>
+        <Categories/>
+        <BestSeller/>
     </div>
   )
 }
