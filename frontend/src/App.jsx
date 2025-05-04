4c5bc1583da54dd12d84c794665ecfa29f252d21
Ananthu M <ananthu.m.utr@gmail.com>
2025-05-04 12:28:10 +0530

    Main banner update

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,9 +1,17 @@
 import React from 'react'
+import { Route, Routes, useLocation } from 'react-router-dom'
 import Navbar from './components/Navbar'
+import Home from './pages/Home'
 const App = () => {
+  const isSellerPath = useLocation().pathname.includes("seller");
   return (
     <div>
-      <Navbar></Navbar>
+      {isSellerPath ? null : <Navbar/>}
+      <div className={`${isSellerPath} ? "" : "px-6 md:px-16 lg:px-24 xl:px-32"`}>
+        <Routes>
+          <Route path='/' element={<Home/>}></Route>
+        </Routes>
+      </div>
     </div>
   )
 }
